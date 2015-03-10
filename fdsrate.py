3c4584356ceeb53e34227bd62af54a6c6d7f34ae
KE Claytor <ke.claytor@gmail.com>
2015-03-09 20:59:10 -0400

    Scraping seems to work. Added in saving so I can debug the results section.

--- fdsrate.py
+++ fdsrate.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 from collections import Counter
 import urllib2
+import pickle
 
 
 # Scrape the main url for the students
@@ -11,7 +12,9 @@ def get_students(base_url):
     soup = BeautifulSoup(page.read())
     # Remove the header with sort by links
     gradcontainer = soup.find('div', {'id':'contentSecond'})
+    # Remove header and footer, leaving the grad info
     gradcontainer.h2.extract()
+    gradcontainer.find('div', {'id':'sidenav'}).extract()
     # Find the links to the grad student pages
     grads = gradcontainer.findAll('a', href=True)
     return grads
@@ -37,7 +40,7 @@ def get_rating(current_student):
         self_fields[key] = 1
     return self_fields
 
-def send_result(nstudents, total_rating, rating):
+def send_rating(nstudents, total_rating, rating):
     print ""
     print "Result for %s: %s" % (rating['name'], rating['email'])
     print "You have filled in:"
@@ -51,14 +54,25 @@ def send_result(nstudents, total_rating, rating):
     return True
 
 if __name__ == "__main__":
-    base_url = 'http://fds.duke.edu/db/aas/Physics/grad'
-    student_urls = get_students(base_url)
-    nstudents = len(student_urls)
-    # First loop through getting all the info for all students
-    totals = Counter({})
-    for current_student in student_urls:
-        rating = get_rating(current_student)
-        totals = totals + rating
+    fname = 'fdsinfo.p'
+    try:
+        fds_file = open(fname, 'r')
+        pickle.load(fds_file)
+        print "Loading saved data..."
+    except IOError:
+        fds_file = open(fname, 'wb')
+        # See if we have pickled data and load that, otherwise scrape
+        base_url = 'http://fds.duke.edu/db/aas/Physics/grad'
+        student_urls = get_students(base_url)
+        nstudents = len(student_urls)
+        # First loop through getting all the info for all students
+        totals = Counter({})
+        for current_student in student_urls:
+            rating = get_rating(current_student)
+            totals = totals + rating
+        pickle.dump(student_urls, fds_file)
+        pickle.dump(totals, fds_file)
+        pickle.dump(nstudents, fds_file)
     # Now go back through the students and e-mail their results
     for current_student in student_urls:
         rating = get_rating(current_student)
