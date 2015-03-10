3e46fefd41e1233a01875f9e15970c36a15bd423
KE Claytor <ke.claytor@gmail.com>
2015-03-09 20:30:47 -0400

    Parsing user fields into a counter (dict) object.

--- fdsrate.py
+++ fdsrate.py
@@ -1,5 +1,6 @@
 # Main FDS Rating script
 from bs4 import BeautifulSoup
+from collections import Counter
 import urllib2
 
 
@@ -18,16 +19,42 @@ def get_rating(current_student):
     student_page = urllib2.urlopen(current_student['href'])
     student_soup = BeautifulSoup(student_page.read())
     info = student_soup.find('div', {'id':'contentSecond'})
-    # The b tag is associated with the information categories
-    info.findAll('b')
+    # We return a dict with info if we have the field
+    self_fields = Counter({})
+    # Student name
+    self_fields['name'] = info.h3.text.split(',')[0]
+    self_fields['email'] = info.select('a[href^=mailto]')[0].text
     # Check for a picture
-    info.findAll('img')
+    img = info.findAll('img')
+    if img:
+        self_fields['img'] = 1
+    # The b tag is associated with the information categories
+    fields = info.findAll('b')
+    for q in fields:
+        key = q.text.strip(':')
+        self_fields[key] = 1
+    return self_fields
 
-
+def send_result(nstudents, total_rating, rating):
+    print ""
+    print "Result for %s: %s" % (rating['name'], rating['email'])
+    print "You have filled in:"
+    for item in total_rating:
+        print "  (%2.2fi\%) %s" % (total_rating[item]/nstudents, item)
+    print "You have neglected:"
+    for item in total_rating:
+        print "  (%2.2fi\%) %s" % (total_rating[item]/nstudents, item)
 
 if __name__ == "__main__":
     base_url = 'http://fds.duke.edu/db/aas/Physics/grad'
     student_urls = get_students(base_url)
+    nstudents = len(student_urls)
+    # First loop through getting all the info for all students
+    totals = Counter({})
     for current_student in student_urls:
         rating = get_rating(current_student)
-        send_rating(current_student, rating)
+        totals = totals + rating
+    # Now go back through the students and e-mail their results
+    for current_student in student_urls:
+        rating = get_rating(current_student)
+        send_rating(nstudents, totals, rating)
